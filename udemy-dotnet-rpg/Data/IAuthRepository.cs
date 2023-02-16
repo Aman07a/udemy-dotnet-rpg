87df90eef3db096fa9dd3b679519447ba7962711
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-16 13:18:12 +0100

    User Registration

--- udemy-dotnet-rpg/Data/IAuthRepository.cs
+++ udemy-dotnet-rpg/Data/IAuthRepository.cs
@@ -3,7 +3,7 @@
 	public interface IAuthRepository
 	{
 		Task<ServiceResponse<int>> Register(User user, string password);
-		Task<ServiceResponse<int>> Login(string username, string password);
+		Task<ServiceResponse<string>> Login(string username, string password);
 		Task<bool> UserExists(string username);
 	}
 }
