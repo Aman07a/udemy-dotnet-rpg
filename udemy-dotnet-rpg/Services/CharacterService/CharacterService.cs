128c5945eb019e09d8495b81a42dd043a5cfb991
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-16 12:33:38 +0100

    POST Implementations

--- udemy-dotnet-rpg/Services/CharacterService/CharacterService.cs
+++ udemy-dotnet-rpg/Services/CharacterService/CharacterService.cs
@@ -28,9 +28,12 @@ namespace udemy_dotnet_rpg.Services.CharacterService
 		{
 			var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
 			var character = _mapper.Map<Character>(newCharacter);
-			character.Id = characters.Max(c => c.Id) + 1;
-			characters.Add(character);
-			serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
+
+			_context.Characters.Add(character);
+			await _context.SaveChangesAsync();
+
+			serviceResponse.Data =
+				await _context.Characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToListAsync();
 			return serviceResponse;
 		}
 
