5634c94ddc992458a626bd98e0f789f709c53dac
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-16 12:20:51 +0100

    ConnectionString & Adding the DbContext

--- udemy-dotnet-rpg/Program.cs
+++ udemy-dotnet-rpg/Program.cs
@@ -2,12 +2,15 @@ global using udemy_dotnet_rpg.Models;
 global using udemy_dotnet_rpg.Services.CharacterService;
 global using udemy_dotnet_rpg.DTOS.Character;
 global using Microsoft.EntityFrameworkCore;
+global using udemy_dotnet_rpg.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
