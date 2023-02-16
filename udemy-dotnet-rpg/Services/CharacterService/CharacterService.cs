1c7fafd328d3d1b094dc7caedf4e7a3786739404
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-16 12:29:42 +0100

    GET Implementations

--- udemy-dotnet-rpg/Services/CharacterService/CharacterService.cs
+++ udemy-dotnet-rpg/Services/CharacterService/CharacterService.cs
@@ -16,9 +16,11 @@ namespace udemy_dotnet_rpg.Services.CharacterService
 		};
 
 		private readonly IMapper _mapper;
+		private readonly DataContext _context;
 
-		public CharacterService(IMapper mapper)
+		public CharacterService(IMapper mapper, DataContext context)
 		{
+			_context = context;
 			_mapper = mapper;
 		}
 
@@ -59,15 +61,16 @@ namespace udemy_dotnet_rpg.Services.CharacterService
 		public async Task<ServiceResponse<List<GetCharacterDTO>>> GetAllCharacters()
 		{
 			var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
-			serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
+			var dbCharacters = await _context.Characters.ToListAsync();
+			serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
 			return serviceResponse;
 		}
 
 		public async Task<ServiceResponse<GetCharacterDTO>> GetCharacterById(int id)
 		{
 			var serviceResponse = new ServiceResponse<GetCharacterDTO>();
-			var character = characters.FirstOrDefault(c => c.Id == id);
-			serviceResponse.Data = _mapper.Map<GetCharacterDTO>(character);
+			var dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+			serviceResponse.Data = _mapper.Map<GetCharacterDTO>(dbCharacter);
 			return serviceResponse;
 		}
 
