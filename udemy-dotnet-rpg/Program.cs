3d08fe8553895674cae3503ff302e44287cbdaa3
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-16 15:41:23 +0100

    Testing Secured Methods with Swagger

--- udemy-dotnet-rpg/Program.cs
+++ udemy-dotnet-rpg/Program.cs
@@ -6,6 +6,8 @@ global using Microsoft.EntityFrameworkCore;
 global using udemy_dotnet_rpg.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +18,18 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+	{
+		Description = """Standard Authorization header using the Bearer scheme. Example: "bearer {token}" """,
+		In = ParameterLocation.Header,
+		Name = "Authorization",
+		Type = SecuritySchemeType.ApiKey
+	});
+
+	c.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<ICharacterService, CharacterService>();
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
@@ -27,7 +40,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 		{
 			ValidateIssuerSigningKey = true,
 			IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8
-					.GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value!)),
+				.GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value!)),
 			ValidateIssuer = false,
 			ValidateAudience = false
 		};
