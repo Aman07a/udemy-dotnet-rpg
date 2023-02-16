f097c9a504abd48d696f84509972a055d3330812
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-16 12:54:46 +0100

    Authentication Repository

---
+++ udemy-dotnet-rpg/Data/IAuthRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace udemy_dotnet_rpg.Data
+{
+	public interface IAuthRepository
+	{
+		Task<ServiceResponse<int>> Register(User user, string password);
+		Task<ServiceResponse<int>> Login(string username, string password);
+		Task<bool> UserExists(string username);
+	}
+}
