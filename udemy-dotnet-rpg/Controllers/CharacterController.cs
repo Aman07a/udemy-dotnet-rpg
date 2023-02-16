f03dcc83f52604cffaeebe58dfe7dc9b53e37888
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-16 11:17:02 +0100

    New Controller & GET a New Character

---
+++ udemy-dotnet-rpg/Controllers/CharacterController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace udemy_dotnet_rpg.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CharacterController : ControllerBase
+	{
+		private static Character knight = new Character();
+
+		[HttpGet]
+		public ActionResult<Character> Get()
+		{
+			return Ok(knight);
+		}
+	}
+}
--- udemy-dotnet-rpg/Program.cs
+++ udemy-dotnet-rpg/Program.cs
@@ -1,3 +1,4 @@
+global using udemy_dotnet_rpg.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
