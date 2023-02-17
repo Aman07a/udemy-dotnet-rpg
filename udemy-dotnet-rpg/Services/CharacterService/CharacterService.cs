99a505926c360bacf7f25ccb14876af9e5a484cc
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-17 08:35:48 +0100

    Proper User-Character Relationship - Add Character

--- udemy-dotnet-rpg/Services/CharacterService/CharacterService.cs
+++ udemy-dotnet-rpg/Services/CharacterService/CharacterService.cs
@@ -29,12 +29,16 @@ namespace udemy_dotnet_rpg.Services.CharacterService
 		{
 			var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
 			var character = _mapper.Map<Character>(newCharacter);
+			character.User = await _context.Users.FirstOrDefaultAsync(u => u.Id == GetUserId());
 
 			_context.Characters.Add(character);
 			await _context.SaveChangesAsync();
 
-			serviceResponse.Data =
-				await _context.Characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToListAsync();
+			serviceResponse.Data = 
+				await _context.Characters
+					.Where(c => c.User!.Id == GetUserId())
+					.Select(c => _mapper.Map<GetCharacterDTO>(c))
+					.ToListAsync();
 			return serviceResponse;
 		}
 
