210714e9bb06f828c596e1aea5cf66fee89a43c3
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-17 08:28:22 +0100

    Proper User-Character Relationship - Get All Characters

--- udemy-dotnet-rpg/Services/CharacterService/CharacterService.cs
+++ udemy-dotnet-rpg/Services/CharacterService/CharacterService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using udemy_dotnet_rpg.DTOS.Character;
@@ -12,12 +13,17 @@ namespace udemy_dotnet_rpg.Services.CharacterService
 	{
 		private readonly IMapper _mapper;
 		private readonly DataContext _context;
+		private readonly IHttpContextAccessor _httpContextAccessor;
 
-		public CharacterService(IMapper mapper, DataContext context)
+		public CharacterService(IMapper mapper, DataContext context, IHttpContextAccessor httpContextAccessor)
 		{
+			_httpContextAccessor = httpContextAccessor;
 			_context = context;
 			_mapper = mapper;
 		}
+
+		private int GetUserId() => int.Parse(_httpContextAccessor.HttpContext!.User
+			.FindFirstValue(ClaimTypes.NameIdentifier)!);
 
 		public async Task<ServiceResponse<List<GetCharacterDTO>>> AddCharacter(AddCharacterDTO newCharacter)
 		{
@@ -59,10 +65,10 @@ namespace udemy_dotnet_rpg.Services.CharacterService
 			return serviceResponse;
 		}
 
-		public async Task<ServiceResponse<List<GetCharacterDTO>>> GetAllCharacters(int userId)
+		public async Task<ServiceResponse<List<GetCharacterDTO>>> GetAllCharacters()
 		{
 			var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
-			var dbCharacters = await _context.Characters.Where(c => c.User!.Id == userId).ToListAsync();
+			var dbCharacters = await _context.Characters.Where(c => c.User!.Id == GetUserId()).ToListAsync();
 			serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
 			return serviceResponse;
 		}
